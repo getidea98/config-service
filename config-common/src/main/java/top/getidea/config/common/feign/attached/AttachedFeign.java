b660851e60ead12b431599448c9e181340fe7f28
苗义民 <getidea98@163.com>
2021-03-04 14:35:12 +0800

    add feign

--- config-common/src/main/java/top/getidea/config/common/feign/attached/AttachedFeign.java
+++ config-common/src/main/java/top/getidea/config/common/feign/attached/AttachedFeign.java
@@ -1,2 +1,24 @@
-package top.getidea.config.common.feign.attached;public interface AttachedFeign {
+package top.getidea.config.common.feign.attached;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.getidea.config.common.entity.assets.Assets;
+import top.getidea.config.common.util.Result;
+
+/**
+ * @author getidea
+ */
+@FeignClient(
+        name = "config-attached"
+)
+@Component
+public interface AttachedFeign {
+    @GetMapping("/api/attached/getAttachedById")
+    Result<Assets> getAttachedById(@RequestParam("id") Integer id);
+
+    @DeleteMapping("/api/attached/deleteAllAssetsOfProduct")
+    Result deleteAllAssetsOfProduct(@RequestParam("id") Integer id, @RequestParam("productVersion") String productVersion);
 }
